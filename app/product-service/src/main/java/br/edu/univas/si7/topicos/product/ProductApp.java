21501aef0366150ef92fd60a0780d631bba49419
Roberto Rocha <rrocha.roberto@gmail.com>
2023-03-14 20:24:35 -0300

    Practices 03 and 04

--- app/product-service/src/main/java/br/edu/univas/si7/topicos/product/ProductApp.java
+++ app/product-service/src/main/java/br/edu/univas/si7/topicos/product/ProductApp.java
@@ -1,13 +1,30 @@
 package br.edu.univas.si7.topicos.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.edu.univas.si7.topicos.product.entities.ProductEntity;
+import br.edu.univas.si7.topicos.product.repository.ProductRepository;
+
 @SpringBootApplication
-public class ProductApp {
+public class ProductApp implements CommandLineRunner {
+
+	@Autowired
+	private ProductRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApp.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// implementar usando os próximos passos
+
+		ProductEntity p1 = new ProductEntity(1, "Arroz", 7.30f, true);
+		repo.save(p1);
+		ProductEntity p2 = new ProductEntity(2, "Feijão", 2.45f, true);
+		repo.save(p2);
+	}
 }
--- app/product-service/src/main/java/br/edu/univas/si7/topicos/product/entities/ProductEntity.java
+++ app/product-service/src/main/java/br/edu/univas/si7/topicos/product/entities/ProductEntity.java
@@ -1,47 +1,25 @@
 package br.edu.univas.si7.topicos.product.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductEntity {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int code;
 	private String name;
 	private float price;
 	private boolean active;
 
-	// criar o construtor com todos os parâmetros
-	// criar os sets e gets
-	public ProductEntity(int code, String name, float price, boolean active) {
-		super();
-		this.code = code;
-		this.name = name;
-		this.price = price;
-		this.active = active;
-	}
-
-	public int getCode() {
-		return code;
-	}
-
-	public void setCode(int code) {
-		this.code = code;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public float getPrice() {
-		return price;
-	}
-
-	public void setPrice(float price) {
-		this.price = price;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
 }
