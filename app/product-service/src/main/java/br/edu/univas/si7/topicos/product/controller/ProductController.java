ea3ea121346c8a25db1734e30215a25e91fd37af
unknown <Administrador@LABF630167.fuvs.br>
2023-03-21 20:26:52 -0300

    Practice 07

--- app/product-service/src/main/java/br/edu/univas/si7/topicos/product/controller/ProductController.java
+++ app/product-service/src/main/java/br/edu/univas/si7/topicos/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.edu.univas.si7.topicos.product.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.univas.si7.topicos.product.dto.ProductDTO;
 import br.edu.univas.si7.topicos.product.entities.ProductEntity;
 import br.edu.univas.si7.topicos.product.repository.ProductRepository;
 import br.edu.univas.si7.topicos.product.support.ObjectNotFoundException;
@@ -30,18 +32,33 @@ public class ProductController {
 
 	@GetMapping("/{code}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ProductEntity getProductById(@PathVariable Integer code) {
+	public ProductDTO getProductById(@PathVariable Integer code) {
 		Optional<ProductEntity> obj = repo.findById(code);
-		if (obj.isPresent()) {
-			return obj.get();
-		}
-		throw new ObjectNotFoundException("Product not found: " + code);
+//		if (obj.isPresent()) {
+//			return obj.get();
+//		}
+//		throw new ObjectNotFoundException("Product not found: " + code);
+		
+		ProductEntity entity = obj.orElseThrow(() -> 
+			new ObjectNotFoundException("Product " + code + " not found"));
+		return new ProductDTO(entity);
 	}
 
 	@GetMapping("/active")
 	@ResponseStatus(HttpStatus.OK)
-	public List<ProductEntity> getAllProductsActive() {
-		return repo.findByActive(true);
+	public List<ProductDTO> getAllProductsActive() {
+//		List<ProductEntity> listEntity = repo.findByActive(true);
+//		List<ProductDTO> listDTO = new ArrayList<>();
+//		for (ProductEntity entity : listEntity) {
+//			ProductDTO dto = new ProductDTO(entity);
+//			listDTO.add(dto);
+//		}
+//		return listDTO;
+		
+		return repo.findByActive(true)
+			.stream()
+			.map(p -> new ProductDTO(p))
+			.collect(Collectors.toList());
 	}
 
 }
