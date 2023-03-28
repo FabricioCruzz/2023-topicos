eccb23c50aba4b5f52f0bc83a2bd2f9e2933af58
Roberto Rocha <rrocha.roberto@gmail.com>
2023-03-28 20:23:51 -0300

    Exercise 02

---
+++ app/product-service/src/test/java/br/edu/univas/si7/topicos/product/service/ProductServiceTest.java
@@ -0,0 +1,48 @@
+package br.edu.univas.si7.topicos.product.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import br.edu.univas.si7.topicos.product.dto.ProductDTO;
+import br.edu.univas.si7.topicos.product.entities.ProductEntity;
+import br.edu.univas.si7.topicos.product.repository.ProductRepository;
+
+public class ProductServiceTest {
+
+	private static ProductRepository repo = Mockito.mock(ProductRepository.class);
+	
+	private ProductService service = new ProductService(repo);
+	
+	@BeforeAll
+	public static void setup() {
+		ProductEntity prod01 = new ProductEntity(1, "test", 0f, false);
+		Mockito.when(repo.findById(1)).thenReturn(Optional.of(prod01));
+		
+		List<ProductEntity> listProd = new ArrayList<>();
+		listProd.add(prod01);
+		Mockito.when(repo.findAll()).thenReturn(listProd);
+	}
+
+	@Test
+	void testGetAllProducts() {
+		List<ProductDTO> allProducts = service.findAll();
+		assertNotNull(allProducts);
+		assertEquals(1, allProducts.size());
+		assertEquals(1, allProducts.get(0).getCode());
+	}
+
+	@Test
+	void testGetProductById() {
+		ProductDTO product = service.findById(1);
+		assertNotNull(product);
+		assertEquals(1, product.getCode());
+	}
+}
