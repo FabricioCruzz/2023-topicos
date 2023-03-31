fa818b9cb80ff66d0948ba8e2112a25eebba7eaa
Roberto Rocha <rrocha.roberto@gmail.com>
2023-03-30 22:07:17 -0300

    Practice 09

--- app/product-service/src/main/java/br/edu/univas/si7/topicos/product/service/ProductService.java
+++ app/product-service/src/main/java/br/edu/univas/si7/topicos/product/service/ProductService.java
@@ -11,35 +11,51 @@ import br.edu.univas.si7.topicos.product.dto.ProductDTO;
 import br.edu.univas.si7.topicos.product.entities.ProductEntity;
 import br.edu.univas.si7.topicos.product.repository.ProductRepository;
 import br.edu.univas.si7.topicos.product.support.ObjectNotFoundException;
+import br.edu.univas.si7.topicos.product.support.ProductException;
 
 @Service
 public class ProductService {
 
 	private ProductRepository repo;
-	
+
 	@Autowired
 	public ProductService(ProductRepository repo) {
 		this.repo = repo;
 	}
 
 	public List<ProductDTO> findAll() {
-		return repo.findAll()
-			.stream()
-			.map(p -> new ProductDTO(p))
-			.collect(Collectors.toList());
-	}
-	
-	public List<ProductDTO> findAllActive() { 
-		return repo.findByActive(true)
-			.stream()
-			.map(p -> new ProductDTO(p))
-			.collect(Collectors.toList());
+		return repo.findAll().stream().map(p -> new ProductDTO(p)).collect(Collectors.toList());
 	}
 
-	public ProductDTO findById(Integer code) {
-		Optional<ProductEntity> obj = repo.findById(code);
-		ProductEntity entity = obj.orElseThrow(
-			() -> new ObjectNotFoundException("Product " + code + " not found"));
-		return new ProductDTO(entity);
+	public List<ProductDTO> findAllActive() {
+		return repo.findByActive(true).stream().map(p -> new ProductDTO(p)).collect(Collectors.toList());
 	}
+
+	public ProductEntity findById(Integer code) {
+		Optional<ProductEntity> obj = repo.findById(code);
+		ProductEntity entity = obj.orElseThrow(() -> new ObjectNotFoundException("Product " + code + " not found"));
+		return entity;
+	}
+
+	public void createProduct(ProductDTO product) {
+		repo.save(toEntity(product));
+	}
+
+	public ProductEntity toEntity(ProductDTO prod) {
+		return new ProductEntity(prod.getCode(), prod.getName(), prod.getPrice(), true);
+	}
+
+	public void updateProduct(ProductEntity product, Integer code) {
+		if (code == null || product == null || code.equals(product.getCode())) {
+			throw new ProductException("Invalid product code.");
+		}
+		ProductEntity existingObj = findById(code);
+		updateData(existingObj, product);
+		repo.save(existingObj);
+	}
+
+	private void updateData(ProductEntity existingObj, ProductEntity obj) {
+		existingObj.setName(obj.getName());
+	}
+
 }
