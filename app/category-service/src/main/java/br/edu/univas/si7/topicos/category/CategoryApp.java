7b104ff7e1df0254f3223c80565252a0eb88dca1
Roberto Rocha <rrocha.roberto@gmail.com>
2023-05-23 19:29:02 -0300

    Including category service

---
+++ app/category-service/src/main/java/br/edu/univas/si7/topicos/category/CategoryApp.java
@@ -0,0 +1,13 @@
+package br.edu.univas.si7.topicos.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryApp.class, args);
+	}
+
+}
---
+++ app/category-service/src/main/java/br/edu/univas/si7/topicos/category/dtos/CategoryDto.java
@@ -0,0 +1,10 @@
+package br.edu.univas.si7.topicos.category.dtos;
+
+import lombok.Data;
+
+@Data
+public class CategoryDto {
+
+    private String name;
+
+}
